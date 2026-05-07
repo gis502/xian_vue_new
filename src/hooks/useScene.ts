c56eca1559eb50a1c8cc91ec2cd2d7655f432390
wzy-warehouse <18135009705@163.com>
2026-05-07 13:59:27 +0800

    修改hooks中store的调用方式

--- src/hooks/useScene.ts
+++ src/hooks/useScene.ts
@@ -4,19 +4,24 @@ import { useLoadingInformationStore } from '@/stores/useLoadingInformation';
 import { useStatusStore } from '@/stores/useStatusStore';
 
 export const useScene = () => {
+  const statusStore = useStatusStore();
+  const loadingInformationStore = useLoadingInformationStore();
+  const leftLegendStore = useLeftLegendStore();
+  const buttonSelectedIdStore = useButtonSelectedIdStore();
+
   // 重置场景
   const resetScene = () => {
     // 重置所有状态
-    useStatusStore().reset();
+    statusStore.reset();
 
     // 重置状态
-    useLoadingInformationStore().resetStatue();
+    loadingInformationStore.resetStatue();
 
     // 重置按钮
-    useButtonSelectedIdStore().resetId();
+    buttonSelectedIdStore.resetId();
 
     // 重置左侧图例
-    useLeftLegendStore().resetLegendListInfo();
+    leftLegendStore.resetLegendListInfo();
   };
 
   return { resetScene };
